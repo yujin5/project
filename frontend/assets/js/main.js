54be03622ca0561977f7ba8dd7813a6f4d65a256
pang-pangho <day_adm@naver.com>
2023-09-11 11:19:57 +0900

    payment

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -234,3 +234,39 @@ buttons.forEach((button) => {
     button.classList.add("active");
   });
 });
+
+// 전화번호 하이픈
+function autoHypenPhone(str) {
+  str = str.replace(/[^0-9]/g, "");
+  var tmp = "";
+  if (str.length < 4) {
+    return str;
+  } else if (str.length < 7) {
+    tmp += str.substr(0, 3);
+    tmp += "-";
+    tmp += str.substr(3);
+    return tmp;
+  } else if (str.length < 11) {
+    tmp += str.substr(0, 3);
+    tmp += "-";
+    tmp += str.substr(3, 3);
+    tmp += "-";
+    tmp += str.substr(6);
+    return tmp;
+  } else {
+    tmp += str.substr(0, 3);
+    tmp += "-";
+    tmp += str.substr(3, 4);
+    tmp += "-";
+    tmp += str.substr(7);
+    return tmp;
+  }
+  return str;
+}
+
+var cellPhone = document.getElementById("cellPhone");
+cellPhone.onkeyup = function (event) {
+  event = event || window.event;
+  var _val = this.value.trim();
+  this.value = autoHypenPhone(_val);
+};
